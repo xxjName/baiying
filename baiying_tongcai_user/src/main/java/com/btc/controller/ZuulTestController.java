11f225b7ac32d8bb1745b6c355788f5eeaf559eb
xxjName <xiexiaojun1995@163.com>
2019-07-09 15:25:03 +0800

    22

--- baiying_tongcai_user/src/main/java/com/btc/controller/ZuulTestController.java
+++ baiying_tongcai_user/src/main/java/com/btc/controller/ZuulTestController.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.btc.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,3 +22,28 @@ public class ZuulTestController {
 }
 
 
+=======
+package com.btc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO: 网关测试类
+ * @author xiexiaojun
+ * @date 2019/7/8 16:42
+ */
+@RestController
+public class ZuulTestController {
+
+    @RequestMapping("getZuulShow")
+    @ResponseBody
+    public String getZuulShow(){
+        return "成功搭建完成!";
+    }
+
+}
+
+
+>>>>>>> dd
--- baiying_tongcai_zuul/src/main/java/com/btc/controller/ZuulTestController.java
+++ baiying_tongcai_zuul/src/main/java/com/btc/controller/ZuulTestController.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.btc.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,3 +28,34 @@ public class ZuulTestController {
 }
 
 
+=======
+package com.btc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * TODO: 网关测试类
+ * @author xiexiaojun
+ * @date 2019/7/8 16:42
+ */
+@RestController
+public class ZuulTestController {
+    /**
+     * TODO: 方法描述
+     * @author xiexiaojun
+     * @date 2019/7/8 18:13
+     * @param name
+     * @return
+     * @throws
+     */
+    @RequestMapping("getZuulShow")
+    @ResponseBody
+    public String getZuulShow(String name){
+        return "成功搭建完成!";
+    }
+
+}
+
+
+>>>>>>> dd
