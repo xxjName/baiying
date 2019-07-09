0c6d2fabe82d45d8515d6091843dbc79ed3d9323
xxjName <xiexiaojun1995@163.com>
2019-07-09 14:33:09 +0800

    decriptions

---
+++ baiying_tongcai_user/src/main/java/com/btc/controller/ZuulTestController.java
@@ -0,0 +1,23 @@
+package com.btc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO: 网关测试类
+ * @author xiexiaojun
+ * @date 2019/7/8 16:42
+ */
+@RestController
+public class ZuulTestController {
+
+    @RequestMapping("getZuulShow")
+    @ResponseBody
+    public String getZuulShow(){
+        return "成功搭建完成!";
+    }
+
+}
+
+
