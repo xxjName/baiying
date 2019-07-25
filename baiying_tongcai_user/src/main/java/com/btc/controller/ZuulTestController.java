25599f02e574e37958ef24f3dd9b21c70fb80b5f
xxjName <xiexiaojun1995@163.com>
2019-07-25 13:49:27 +0800

    提交redis

--- baiying_tongcai_user/src/main/java/com/btc/controller/ZuulTestController.java
+++ baiying_tongcai_user/src/main/java/com/btc/controller/ZuulTestController.java
@@ -2,7 +2,9 @@ package com.btc.controller;
 
 import com.btc.feigin.ProductFeigin;
 import com.btc.main.Product;
+import com.btc.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,20 +15,26 @@ import java.util.Map;
 
 /**
  * TODO: 网关测试类
+ *
  * @author xiexiaojun
  * @date 2019/7/8 16:42
  */
 @RestController
 public class ZuulTestController {
- @Autowired
- ProductFeigin productFeigin;
+    @Autowired
+    ProductFeigin productFeigin;
+   @Autowired
+    RedisService redisService;
 
     @RequestMapping("getZuulShow")
     @ResponseBody
-    public List<Product>  getZuulShow(){
-      Map<String,Object> map=new HashMap<String, Object>();
-
-         return productFeigin.getProductList(map);
+    public List<Product> getZuulShow() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("33","333");
+        redisService.set("ddd","dddd");
+       String name= redisService.get("ddd");
+        System.out.println("==========================="+name);
+        return productFeigin.getProductList(map);
     }
 
 }
